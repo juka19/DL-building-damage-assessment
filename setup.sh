6a3d20b191d31972197130cf7a4db32c817b2ad7
Julian Kath <56269720+juka19@users.noreply.github.com>
2023-05-05 16:41:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ source activate thesis
 # conda install -f environment.yml
 
 # execute scripts
-python src/01_get_maxar_links.py --out_dir $1
-#python src/02_download_tifs.py --metadata $1/metadata.csv --out_dir $1/tifs
+python src/data_prep/01_get_maxar_links.py --out_dir $1
+python src/data_prep/02_download_tifs.py --metadata $1/data/metadata.csv --out_dir $1/tifs
 for area in $(ls copernicus)
 do
     aoi='(AOI.{2})'
@@ -12,6 +12,6 @@ do
     then
         aoi_name=${BASH_REMATCH[1]}
     fi
-    python src/03_prep_building_footprints.py --zipfile_path copernicus/$area --extract_dir $1/$aoi_name --dest_dir dest_buildings/hotosm_tur_destroyed_buildings_polygons_geojson.geojson --out_dir $1/footprints/$aoi_name.geojson
-    python src/04_create_imgs_msks.py --metadata $1/metadata_gdf.geojson --aoi_zipfile copernicus/$area --footprint_file $1/footprints/$aoi_name.geojson --out_dir $1/$aoi_name --extract_dir $1/$aoi_name --img_folder $1/tifs/visual/
+    python src/data_prep/03_prep_building_footprints.py --zipfile_path copernicus/$area --extract_dir $1/$aoi_name --dest_dir dest_buildings/hotosm_tur_destroyed_buildings_polygons_geojson.geojson --out_dir $1/footprints/$aoi_name.geojson
+    python src/data_prep/04_raster_processing.py --metadata $1/data/metadata_gdf.geojson --aoi_zipfile copernicus/$area --footprint_file $1/data/footprints/$aoi_name.geojson --out_dir $1/$aoi_name --extract_dir $1/$aoi_name --img_folder $1/tifs/visual/ --data_mask_dir $1/masks/ --root_dir $1 --data_tifs $1/data_tifs
 done
